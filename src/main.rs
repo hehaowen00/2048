745ae0c96dd20c0525be235ce2c2caab7ccb5493
hehaowen00 <edmundhe@outlook.com>
2021-12-05 04:45:33 +1000

    Implement Expectimax AI player

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ mod action;
 mod game;
 mod grid;
 mod player;
+mod solver;
 mod util;
 
 use game::Game;
 use player::Human;
+use solver::Expectimax;
 
 fn main() {
-    let mut game = Game::new(Human::new());
+    // let mut game = Game::new(Human::new());
+    let mut game = Game::new(Expectimax::new());
     game.run();
 }
---
+++ src/solver.rs
@@ -0,0 +1,111 @@
+use crate::action::{Action, Direction};
+use crate::grid::Grid;
+use crate::player::Player;
+use crate::util::dot_product;
+
+pub struct Expectimax {
+    stack: Vec<Grid>,
+}
+
+impl Expectimax {
+    pub fn new() -> Self {
+        Self {
+            stack: Vec::new(),
+        }
+    }
+
+    pub fn max(&mut self, grid: &Grid, depth: u16, max_depth: u16) -> (Option<Direction>, f64) {
+        if depth == max_depth {
+            return (None, Self::heuristic(grid));
+        }
+
+        let actions = grid.get_actions();
+        let mut best_action = None;
+        let mut best_score = f64::NEG_INFINITY;
+
+        for action in actions {
+            if best_action.is_none() {
+                best_action = Some(action);
+            }
+
+            let mut copy = grid.clone();
+            copy.shift(action);
+
+            let score = self.chance(&copy, depth + 1, max_depth);
+            if score > best_score {
+                best_action = Some(action);
+                best_score = score;
+            }
+        }
+
+        (best_action, best_score)
+    }
+
+    fn chance(&mut self, grid: &Grid, depth: u16, max_depth: u16) -> f64 {
+        if depth == max_depth {
+            return Self::heuristic(grid);
+        }
+
+        let mut score = 0.0;
+        let mut copy = grid.clone();
+
+        let empty = copy.get_empty();
+        let count = empty.len();
+
+        for (i, j) in empty {
+            copy.cells[i][j] = 2;
+            let (_, score_2) = self.max(&copy, depth + 1, max_depth);
+            let weighted = (0.9 * score_2) / (count as f64);
+            score += weighted;
+
+            copy.cells[i][j] = 4;
+            let (_, score_4) = self.max(&copy, depth + 1, max_depth);
+            let weighted = (0.1 * score_4) / (count as f64);
+            score += weighted;
+
+            copy.cells[i][j] = 0;
+        }
+
+        score
+    }
+
+    fn heuristic(grid: &Grid) -> f64 {
+        let moves = grid.get_actions();
+        if moves.len() == 0 {
+            return f64::NEG_INFINITY;
+        }
+
+        let empty = grid.get_empty();
+        let len = empty.len();
+
+        const WEIGHTS: [[f64; 4]; 4] = [
+            [16.0,15.0,14.0,13.0],
+            [9.0,10.0,11.0,12.0],
+            [8.0,7.0,6.0,5.0],
+            [1.0,2.0,3.0,4.0]
+        ];
+
+        (len * len) as f64 + dot_product(&grid.cells, &WEIGHTS)
+    }
+
+}
+
+impl Player for Expectimax {
+    fn next_action(&mut self, grid: &Grid) -> Option<Action> {
+        let empty = grid.get_empty();
+        let max_depth = if empty.len() > 7 {
+            4
+        } else {
+            8
+        };
+
+        let (action, _) = self.max(grid, 0, max_depth);
+        match action {
+            Some(direction) => {
+                println!("Action: {:?}", direction);
+                Some(Action::Move(direction))
+            },
+            _ => None,
+        }
+    }
+}
