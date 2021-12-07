77885465ccad0b1132a42fd2236cc2a678f9db49
hehaowen00 <edmundhe@outlook.com>
2021-12-08 01:23:50 +1000

    Add more scoring methods

--- src/solver.rs
+++ src/solver.rs
@@ -1,7 +1,7 @@
 use crate::action::{Action, Direction};
 use crate::grid::Grid;
 use crate::player::Player;
-use crate::util::dot_product;
+use crate::util::{dot_product, transpose};
 
 pub struct Expectimax;
 
@@ -13,13 +13,7 @@ impl Expectimax {
 
 impl Player for Expectimax {
     fn next_action(&mut self, grid: &Grid) -> Option<Action> {
-        let empty = grid.get_empty();
-        let depth = if empty.len() > 7 {
-            4
-        } else {
-            8
-        };
-
+        let depth = 6;
         let (action, _) = max(grid, depth);
 
         match action {
@@ -86,21 +80,124 @@ fn chance(grid: &Grid, depth: u16) -> f64 {
 }
 
 fn heuristic(grid: &Grid) -> f64 {
-    let moves = grid.get_actions();
+    let corner_max = corner_max(grid);
+    let empty_tiles = empty_tiles(grid);
+    let monotonicity = monotonicity(grid);
+    let smoothness = smoothness(grid);
+    let weighted_sum = weighted_sum(grid);
+    let moves_count = moves_count(grid);
 
-    if moves.len() == 0 {
-        return f64::NEG_INFINITY;
+    corner_max + empty_tiles +  monotonicity +
+    moves_count + smoothness + weighted_sum
+}
+
+fn corner_max(grid: &Grid) -> f64 {
+    let mut max_value = 0;
+    
+    for row in grid.cells {
+        for el in row {
+            if el > max_value {
+                max_value = el;
+            }
+        }
     }
 
+    match grid.cells[0][0] == max_value {
+        true => max_value as f64,
+        false => 0.0,
+    }
+}
+
+fn empty_tiles(grid: &Grid) -> f64 {
     let empty = grid.get_empty();
     let len = empty.len();
 
+    (len * len) as f64
+}
+
+fn monotonicity(grid: &Grid) -> f64 {
+    let mut score = 0.0;
+
+    for row in grid.cells {
+        let mut prev = row[0] as f64;
+        for el in &row[1..] {
+            let el = *el as f64;
+            if el == prev {
+                score += 1.0;
+            }
+            prev = el;
+        }
+    }
+
+    let mut g2 = grid.clone();
+    transpose(&mut g2.cells);
+
+    for row in g2.cells {
+        let mut prev = row[0] as f64;
+        for el in &row[1..] {
+            let el = *el as f64;
+            if el == prev {
+                score += 1.0;
+            }
+            prev = el;
+        }
+    }
+
+    score
+}
+
+fn moves_count(grid: &Grid) -> f64 {
+    let moves = grid.get_actions();
+    let len = moves.len();
+
+    match len {
+        0 => f64::NEG_INFINITY,
+        _ => (len * len) as f64
+    }
+}
+
+fn smoothness(grid: &Grid) -> f64 {
+    let mut score = 0.0;
+
+    for row in grid.cells {
+        let mut prev = row[0] as f64;
+        for el in &row[1..] {
+            let el = *el as f64;
+            if prev == 0.0 || el == 0.0 {
+                prev = el;
+                continue;
+            }
+            score -= (prev - el).abs();
+            prev = el;
+        }
+    }
+
+    let mut g2 = grid.clone();
+    transpose(&mut g2.cells);
+
+    for row in g2.cells {
+        let mut prev = row[0] as f64;
+        for el in &row[1..] {
+            let el = *el as f64;
+            if prev == 0.0 || el == 0.0 {
+                prev = el;
+                continue;
+            }
+            score -= (prev - el).abs();
+            prev = el;
+        }
+    }
+
+    score
+}
+
+fn weighted_sum(grid: &Grid) -> f64 {
     const WEIGHTS: [[f64; 4]; 4] = [
         [13.0,8.0,5.0,3.0],
-        [0.0,1.0,1.0,2.0],
+        [0.0,0.5,1.0,2.0],
         [-0.25,-0.5,-0.75,-1.25],
         [-8.5,-5.25,-3.25,-2.0],
     ];
 
-    (len * len) as f64 + dot_product(&grid.cells, &WEIGHTS)
+    dot_product(&grid.cells, &WEIGHTS)
 }
